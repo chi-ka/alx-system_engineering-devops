338855264be001830425cd8ebc8c17ccea85d126
Chika Okafor <ochika6363@gmail.com>
2024-05-15 12:59:44 -0700

    done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -32,16 +32,19 @@ def dictionary_of_list_of_dictionaries():
     for user in user_data:
         employee_id = user['id']
         USERNAME = user['username']
-        todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+        todo_url = (
+                f"https://jsonplaceholder.typicode.com/todos"
+                f"?userId={employee_id}"
+                )
         todo_response = requests.get(todo_url)
         todo_data = todo_response.json()
 
         output = []
         for task in todo_data:
             dic = {}
-            dic["username"]= USERNAME
-            dic["task"]= task['title']
-            dic["completed"]= task['completed']
+            dic["username"] = USERNAME
+            dic["task"] = task['title']
+            dic["completed"] = task['completed']
             output.append(dic)
 
         output_dic[employee_id] = output
